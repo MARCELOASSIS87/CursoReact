e0b0cfc81f4e77a967e407b98a777f16d46699ea
Marcelo Assis <Marceloassis50@gmail.com>
2022-05-16 18:17:19 -0300

    Usando imagens das duas pastas

--- secao_3/src/App.js
+++ secao_3/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Fotopessoal2 from './Assets/fotopessoal2.jpg'
 function App() {
   return (
     <div className="App">
-      
+        <h1>Avançando em React/ Seção 3</h1>
+        {/* Imagem em Public*/}
+        <div>
+          <img src="/fotopessoal.jpg" alt="Foto sem Documento" />
+        </div>
+        {/* Imagem em Public*/}
+        <div>
+          <img src={Fotopessoal2} alt="Foto pessoal 2" />
+        </div>
     </div>
   );
 }
