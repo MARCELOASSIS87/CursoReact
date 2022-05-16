13664f35fb3e94af930055a8e5f1ef24cd1091dd
Marcelo Assis <Marceloassis50@gmail.com>
2022-05-16 17:46:08 -0300

    alterações finais

--- Fundamentos/fundamentos_react/src/App.js
+++ Fundamentos/fundamentos_react/src/App.js
@@ -2,15 +2,18 @@ import FirstComponent from './components/FirstComponent';
 import './App.css';
 import TemplateExpressions from './components/TemplateExpressions';
 import Events from './components/Events';
+import Challenge from './components/Challenge';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <h2>Aqui o componente:</h2>
+      <Challenge />
       <FirstComponent />
       <TemplateExpressions />
       <Events />
+      
     </div>
   );
 }
--- Fundamentos/fundamentos_react/src/components/Challenge.js
+++ Fundamentos/fundamentos_react/src/components/Challenge.js
@@ -1,8 +1,39 @@
-const challenge = () => {
-    const handleChallenge = (e) => {
+const Challenge = () => {
+
+    const latitude = 20.2;
+    const longitude = 31.2;
+
+    const handleMyEvent = (e) => {
         console.log(e);
-        
+        { Soma(latitude, longitude) };
 
         console.log("Ativou o evento");
     };
-}
+
+    const Soma = (a, b) => {
+        const soma = (a + b);
+        return (
+            <div>
+                <h2>{Soma}</h2>
+                <p>{console.log("A soma é: %d ", soma)}</p>
+            </div >
+
+        )
+    };
+    return (
+        <div>
+            <div>
+                <h2>Número 1 = {latitude}</h2>
+                <h2>Número 2 = {longitude}</h2>
+
+                <button onClick={handleMyEvent}>Soma dos números</button>
+
+            </div>
+        </div>
+
+
+    )
+
+};
+
+export default Challenge;
