de4b9a92435ee9cda5846b05f487aa55991149f3
Marcelo Assis <Marceloassis50@gmail.com>
2022-05-16 20:48:04 -0300

    inserção do delete

--- secao_3/src/components/ListRender.js
+++ secao_3/src/components/ListRender.js
@@ -3,11 +3,21 @@ import { useState } from 'react'
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Josias"]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, name:"MArcelo", age: 34},
         {id: 2, name:"Mazinho", age: 12},
         {id: 3, name:"Heelna", age: 14},
     ]);
+
+     
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+        setUsers((prevUsers) =>{
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        });
+    };
+    
+    
 
     return (
         <div>
@@ -23,6 +33,7 @@ const ListRender = () => {
                     </li>
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete random user</button>
         </div>
     )
 }
