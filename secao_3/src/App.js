114f6ca38c6e85b70b9e4c6a236320b2f5a6958f
Marcelo Assis <Marceloassis50@gmail.com>
2022-05-16 21:16:48 -0300

    Props aprendizado

--- secao_3/src/App.js
+++ secao_3/src/App.js
@@ -1,23 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 import Fotopessoal2 from './Assets/fotopessoal2.jpg'
+import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManagerData from './components/ManagerData';
+import ShowUserName from './components/ShowUserName';
 function App() {
+  const name = "Pitchulo";
+  const [userName] = useState("Maria");
+
   return (
     <div className="App">
-        <h1>Avançando em React/ Seção 3</h1>
-        {/* Imagem em Public*/}
-        <div>
-          <img src="/fotopessoal.jpg" alt="Foto sem Documento" />
-        </div>
-        {/* Imagem em Public*/}
-        <div>
-          <img src={Fotopessoal2} alt="Foto pessoal 2" />
-        </div>
-        <ManagerData/>
-        <ListRender/>
+      <h1>Avançando em React/ Seção 3</h1>
+      {/* Imagem em Public*/}
+      <div>
+        <img src="/fotopessoal.jpg" alt="Foto sem Documento" />
+      </div>
+      {/* Imagem em Public*/}
+      <div>
+        <img src={Fotopessoal2} alt="Foto pessoal 2" />
+      </div>
+      <ManagerData />
+      <ListRender />
+      <ConditionalRender />
+      <ShowUserName name="Marcelo" /**Direto*/ />
+      <ShowUserName name={name} /**Pela variável*/ />
+      <ShowUserName name={userName} /**Pelo useState*/ />
     </div>
-    
+
   );
 }
 
