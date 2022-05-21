89dc747e5fa5ecc94eed0c87228de9c13712b5b4
Marcelo Assis <Marceloassis50@gmail.com>
2022-05-21 19:03:52 -0300

    react_css completo

--- react_css/src/App.js
+++ react_css/src/App.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
+  const n = 15
+  const [name] = useState("Marcelo")
+  const redTitle = false;
   return (
     <div className="App">
       {/**CSS global */}
@@ -10,9 +15,26 @@ function App() {
       <MyComponent />
       <p>este é o parágrafo do App.js</p>
       {/*inline CSS*/}
-      <p style={{ color: 'blue', padding: '25px', borderTop: "2px solid red" }}>
+      <p style={{
+        color: 'blue',
+        padding: '25px',
+        borderTop: "2px solid red"
+      }}
+      >
         Este elemento foi estilizado de forma inline
       </p>
+      {/** CSS inline dinâmico*/}
+      <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: 'pink' })}>
+        CSS dinâmico</h2>
+      <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: 'pink' })}>
+        CSS dinâmico</h2>
+      <h2 style={name === "Marcelo" ? { color: "green", backgroundColor:"#000" } : null }>
+        CSS dinâmico</h2>
+      {/**Classes dinâmicas no CSS */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter clase dinâmica</h2>  
+      {/** CSS modules */}
+      <Title />  
     </div>
   );
 }
